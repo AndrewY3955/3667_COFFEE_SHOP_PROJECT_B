a1f537cbe8d314c1d32ac5db569919709308f715
Andrew Yates <133114111+AndrewY3955@users.noreply.github.com>
2025-10-22 11:57:04 -0400

    Update Caramel.java

--- src/main/java/Decorator/Caramel.java
+++ src/main/java/Decorator/Caramel.java
@@ -2,37 +2,34 @@ package main.java.Decorator;
 
 import main.java.Base.Coffee;
 
-public class Caramel extends CondimentDecorator {
-    private double price = 0.30;
+public class Caramel extends CondimentDecorator 
+{
+    private Coffee coffee;
 
-    public Caramel(Coffee coffee) {
-        super(coffee);
+    public Caramel(Coffee coffee) 
+	{
+        this.coffee = coffee;
     }
 
     @Override
-    public String getDescription() {
+    public String getDescription() 
+	{
         return coffee.getDescription() + ", Caramel";
     }
 
     @Override
-    public double cost(String size) {
-        double coffeeSizeMultiplier = getSizeMultiplier(size);
-
-        double sumOfAllCondiments = price;
-        if (coffee instanceof CondimentDecorator) {
-            sumOfAllCondiments += ((CondimentDecorator) coffee).getCondimentSum();
-        }
-
-        double baseCoffeeCost = coffee.cost(size) - (sumOfAllCondiments * coffeeSizeMultiplier);
-        return ((baseCoffeeCost * coffeeSizeMultiplier) + (sumOfAllCondiments * coffeeSizeMultiplier));
+    public double cost(String size) 
+	{
+        double add = 0.30;
+        return coffee.cost(size) + (add * getSizeMultiplier(size));
     }
 
-    @Override
-    public double getCondimentSum() {
-        double sum = price;
-        if (coffee instanceof CondimentDecorator) {
-            sum += ((CondimentDecorator) coffee).getCondimentSum();
+    private double getSizeMultiplier(String size) 
+	{
+        switch (size) {
+            case "Medium": return 1.2;
+            case "Large": return 1.4;
+            default: return 1.0;
         }
-        return sum;
     }
 }
