d6c8fc325739232125a8afcf33244ed0fde80f1f
Andrew Yates <133114111+AndrewY3955@users.noreply.github.com>
2025-10-22 11:57:19 -0400

    Update Milk.java

--- src/main/java/Decorator/Milk.java
+++ src/main/java/Decorator/Milk.java
@@ -2,37 +2,35 @@ package main.java.Decorator;
 
 import main.java.Base.Coffee;
 
-public class Milk extends CondimentDecorator {
-    private double price = 0.20;
+/** Adds Milk to a coffee */
+public class Milk extends CondimentDecorator 
+{
+    private Coffee coffee;
 
-    public Milk(Coffee coffee) {
-        super(coffee);
+    public Milk(Coffee coffee) 
+	{
+        this.coffee = coffee;
     }
 
     @Override
-    public String getDescription() {
+    public String getDescription() 
+    {
         return coffee.getDescription() + ", Milk";
     }
 
     @Override
-    public double cost(String size) {
-        double coffeeSizeMultiplier = getSizeMultiplier(size);
-
-        double sumOfAllCondiments = price;
-        if (coffee instanceof CondimentDecorator) {
-            sumOfAllCondiments += ((CondimentDecorator) coffee).getCondimentSum();
-        }
-
-        double baseCoffeeCost = coffee.cost(size) - (sumOfAllCondiments * coffeeSizeMultiplier);
-        return ((baseCoffeeCost * coffeeSizeMultiplier) + (sumOfAllCondiments * coffeeSizeMultiplier));
+    public double cost(String size) 
+    {
+        double add = 0.20;
+        return coffee.cost(size) + (add * getSizeMultiplier(size));
     }
 
-    @Override
-    public double getCondimentSum() {
-        double sum = price;
-        if (coffee instanceof CondimentDecorator) {
-            sum += ((CondimentDecorator) coffee).getCondimentSum();
+    private double getSizeMultiplier(String size) 
+    {
+        switch (size) {
+            case "Medium": return 1.2;
+            case "Large": return 1.4;
+            default: return 1.0;
         }
-        return sum;
     }
 }
