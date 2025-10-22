8441c598a56875d15965333cdac625f5e66daeb7
Andrew Yates <133114111+AndrewY3955@users.noreply.github.com>
2025-10-22 11:58:20 -0400

    Update Whip.java

--- src/main/java/Decorator/Whip.java
+++ src/main/java/Decorator/Whip.java
@@ -2,37 +2,34 @@ package main.java.Decorator;
 
 import main.java.Base.Coffee;
 
-public class Whip extends CondimentDecorator {
-    private double price = 0.15;
+public class Vanilla extends CondimentDecorator 
+{
+    private Coffee coffee;
 
-    public Whip(Coffee coffee) {
-        super(coffee);
+    public Vanilla(Coffee coffee) 
+	{
+        this.coffee = coffee;
     }
 
     @Override
-    public String getDescription() {
-        return coffee.getDescription() + ", Whip";
+    public String getDescription() 
+	{
+        return coffee.getDescription() + ", Vanilla";
     }
 
     @Override
-    public double cost(String size) {
-        double coffeeSizeMultiplier = getSizeMultiplier(size);
+    public double cost(String size) 
+	{
+        double add = 0.25;
+        return coffee.cost(size) + (add * getSizeMultiplier(size));
+    }
 
-        double sumOfAllCondiments = price;
-        if (coffee instanceof CondimentDecorator) {
-            sumOfAllCondiments += ((CondimentDecorator) coffee).getCondimentSum();
+    private double getSizeMultiplier(String size) 
+	{
+        switch (size) {
+            case "Medium": return 1.2;
+            case "Large": return 1.4;
+            default: return 1.0;
         }
-
-        double baseCoffeeCost = coffee.cost(size) - (sumOfAllCondiments * coffeeSizeMultiplier);
-        return ((baseCoffeeCost * coffeeSizeMultiplier) + (sumOfAllCondiments * coffeeSizeMultiplier));
-    }
-
-    @Override
-    public double getCondimentSum() {
-        double sum = price;
-        if (coffee instanceof CondimentDecorator) {
-            sum += ((CondimentDecorator) coffee).getCondimentSum();
-        }
-        return sum;
     }
 }
