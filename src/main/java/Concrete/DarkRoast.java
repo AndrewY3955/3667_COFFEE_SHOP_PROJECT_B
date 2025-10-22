babc18b166743e38992a4899ee680e8e6256ac74
Andrew Yates <133114111+AndrewY3955@users.noreply.github.com>
2025-10-22 12:01:48 -0400

    Update DarkRoast.java

--- src/main/java/Concrete/DarkRoast.java
+++ src/main/java/Concrete/DarkRoast.java
@@ -2,22 +2,26 @@ package main.java.Concrete;
 
 import main.java.Base.Coffee;
 
-public class DarkRoast extends Coffee {
+/** Concrete Coffee: Dark Roast */
+public class DarkRoast extends Coffee 
+{
 
-    public DarkRoast() {
+    public DarkRoast() 
+    {
         description = "Dark Roast";
     }
 
     @Override
-    public double cost(String size) {
+    public double cost(String size) 
+    {
         double base = 1.60;
-        double coffeeSizeMultiplier = getSizeMultiplier(size);
-        double sumOfCondiments = 0.0;
-        return ((base * coffeeSizeMultiplier) + ((sumOfCondiments) * coffeeSizeMultiplier));
+        return base * getSizeMultiplier(size);
     }
 
-    protected double getSizeMultiplier(String size) {
-        switch (size) {
+    private double getSizeMultiplier(String size) 
+    {
+        switch (size) 
+        {
             case "Medium": return 1.2;
             case "Large": return 1.4;
             default: return 1.0;
