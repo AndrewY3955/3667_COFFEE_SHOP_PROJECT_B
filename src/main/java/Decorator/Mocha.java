b07828fbaa59e2c1ef4eff37b47d4d5dae17d406
Andrew Yates <133114111+AndrewY3955@users.noreply.github.com>
2025-10-22 11:57:31 -0400

    Update Mocha.java

--- src/main/java/Decorator/Mocha.java
+++ src/main/java/Decorator/Mocha.java
@@ -3,36 +3,32 @@ package main.java.Decorator;
 import main.java.Base.Coffee;
 
 public class Mocha extends CondimentDecorator {
-    private double price = 0.35;
+    private Coffee coffee;
 
-    public Mocha(Coffee coffee) {
-        super(coffee);
+    public Mocha(Coffee coffee) 
+	{
+        this.coffee = coffee;
     }
 
     @Override
-    public String getDescription() {
+    public String getDescription() 
+	{
         return coffee.getDescription() + ", Mocha";
     }
 
     @Override
-    public double cost(String size) {
-        double coffeeSizeMultiplier = getSizeMultiplier(size);
-
-        double sumOfAllCondiments = price;
-        if (coffee instanceof CondimentDecorator) {
-            sumOfAllCondiments += ((CondimentDecorator) coffee).getCondimentSum();
-        }
-
-        double baseCoffeeCost = coffee.cost(size) - (sumOfAllCondiments * coffeeSizeMultiplier);
-        return ((baseCoffeeCost * coffeeSizeMultiplier) + (sumOfAllCondiments * coffeeSizeMultiplier));
+    public double cost(String size) 
+	{
+        double add = 0.35;
+        return coffee.cost(size) + (add * getSizeMultiplier(size));
     }
 
-    @Override
-    public double getCondimentSum() {
-        double sum = price;
-        if (coffee instanceof CondimentDecorator) {
-            sum += ((CondimentDecorator) coffee).getCondimentSum();
+    private double getSizeMultiplier(String size) 
+	{
+        switch (size) {
+            case "Medium": return 1.2;
+            case "Large": return 1.4;
+            default: return 1.0;
         }
-        return sum;
     }
 }
